25cb72870d45c26fc0dcb02f2c98a481966db7e0
Swayambhu Prasad Panda <swayambhuprasadpanda@gmail.com>
2022-11-17 16:14:45 +0530

    Text Animation

---
+++ Text-Animation/script.js
@@ -0,0 +1,23 @@
+const texth1 = document.getElementById('text')
+const speedEle = document.getElementById('speed')
+const text = 'i love Ani...';
+let index = 0;
+let speed = 300 / speedEle.value;
+
+writetext();
+
+function writetext() {
+    texth1.innerHTML = text.slice(0, index)
+
+    index++;
+
+    if (index > text.length) {
+        index = 1;
+    }
+
+    setTimeout(writetext, speed);
+}
+
+speedEle.addEventListener('input', (e) => {
+    speed = (300 / e.target.value);
+})
